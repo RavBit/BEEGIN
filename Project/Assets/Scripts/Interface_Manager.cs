34f034d4570854580c04903abc5494743df03368
Sergi van Ravenswaay <sergi@live.nl>
2018-06-05 11:19:42 +0200

    dotween and start home screen

---
+++ Project/Assets/Scripts/Interface_Manager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Interface_Manager : MonoBehaviour {
+    [Header("Toolbar Components")]
+    public Image Toolbar;
+    private bool toggled = false;
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+
+    public void OnToggle()
+    {
+        toggled = !toggled;
+
+    }
+}

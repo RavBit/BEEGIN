d78e8e0c454b426f3596474684de0ed1b4144ae3
Sergi van Ravenswaay <sergi@live.nl>
2018-06-05 12:57:43 +0200

    Sidebar

--- Project/Assets/Scripts/Interface_Manager.cs
+++ Project/Assets/Scripts/Interface_Manager.cs
@@ -2,20 +2,30 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using DG.Tweening;
 
 public class Interface_Manager : MonoBehaviour {
     [Header("Toolbar Components")]
     public Image Toolbar;
+    [Range(0, 1)]
+    [SerializeField]
+    private float ToolbarSpeed = 0.5f;
     private bool toggled = false;
 	// Use this for initialization
 	void Start () {
-		
 	}
 
 
     public void OnToggle()
     {
         toggled = !toggled;
-
+        if(toggled)
+        {
+            Toolbar.gameObject.transform.DOMoveX(0, 1);
+        }
+        if (!toggled)
+        {
+            Toolbar.gameObject.transform.DOMoveX(-700, 1);
+        }
     }
 }

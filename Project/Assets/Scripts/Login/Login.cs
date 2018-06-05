96d2c42a1048d3e038ddeb9002a1b417765069b6
Sergi van Ravenswaay <sergi@live.nl>
2018-06-05 11:09:47 +0200

    Login screen working

---
+++ Project/Assets/Scripts/Login/Login.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Login : MonoBehaviour {
+    public void LoginUser()
+    {
+        SceneManager.LoadScene("Home");
+    }
+}

7be76a14d740fdb2c358c4d581231ef0037bd78b
Sergi van Ravenswaay <sergi@live.nl>
2018-06-12 12:48:14 +0200

    Hub working

--- Project/Assets/Scripts/Interface_Manager.cs
+++ Project/Assets/Scripts/Interface_Manager.cs
@@ -6,14 +6,21 @@ using DG.Tweening;
 
 public class Interface_Manager : MonoBehaviour {
     [Header("Toolbar Components")]
-    public Image Toolbar;
+    public Transform Toolbar;
+    public Transform Menubar;
+    public Transform[] Hexagons;
+    public Transform Menu_Background;
     [Range(0, 1)]
     [SerializeField]
     private float ToolbarSpeed = 0.5f;
     private bool toggled = false;
+
+    public int i = 0;
 	// Use this for initialization
 	void Start () {
-	}
+        Menu_Background.GetComponent<Image>().DOFade(0, 0.01F);
+
+    }
 
 
     public void OnToggle()
@@ -21,11 +28,15 @@ public class Interface_Manager : MonoBehaviour {
         toggled = !toggled;
         if(toggled)
         {
-            Toolbar.gameObject.transform.DOMoveX(0, 1);
+            Menubar.DORotate(new Vector3(0, 0, 90), 1);
+            Menu_Background.GetComponent<Image>().DOFade(1, 1F);
+            Toolbar.DOMoveX(-250, 1);
         }
         if (!toggled)
         {
-            Toolbar.gameObject.transform.DOMoveX(-700, 1);
+            Menubar.DORotate(Vector3.zero, 1);
+            Menu_Background.GetComponent<Image>().DOFade(0, 1F);
+            Toolbar.DOMoveX(-1000, 1);
         }
     }
 }
---
+++ Project/Assets/Scripts/TestRotation.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestRotation : MonoBehaviour
+{
+
+    float f_lastX = 0.0f;
+    float f_difX = 0.5f;
+    float f_steps = 0.0f;
+    int i_direction = 1;
+
+    public float z;
+    public Quaternion rot;
+    public int rotSpeed;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Touch touch = Input.GetTouch(0);
+        Debug.Log("touch " + touch.position);
+            if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Moved)
+            {
+                Vector2 touchDeltaPosition = Input.GetTouch(0).deltaPosition;
+                z -= touchDeltaPosition.x * rotSpeed * Time.deltaTime;
+                rot = Quaternion.Euler(0, 0, z);
+                transform.rotation = rot;
+            }
+    }
+}

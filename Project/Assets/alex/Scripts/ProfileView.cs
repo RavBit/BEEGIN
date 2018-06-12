eba9353067687d3d0df8e518cb18438879b815b0
hkulex <31885235+hkulex@users.noreply.github.com>
2018-06-12 17:33:35 +0200

    - Hallo sergi

--- Project/Assets/alex/Scripts/ProfileView.cs
+++ Project/Assets/alex/Scripts/ProfileView.cs
@@ -11,9 +11,12 @@ public class ProfileView : MonoBehaviour
     [SerializeField] private Text _labelPun;
     [SerializeField] private Button _buttonClose;
     [SerializeField] private Image _imageCrown;
-    
 
-    public void Initialize(Item[] items, string profession, string pun, bool isOwner)
+    [SerializeField] private Button _buttonAccept;
+    [SerializeField] private Button _buttonDecline;
+
+
+    public void Initialize(Item[] items, string profession, string pun, bool isOwner, Profile profile)
     {
         for (int i = 0; i < items.Length; i++)
         {
@@ -27,11 +30,25 @@ public class ProfileView : MonoBehaviour
         _labelPun.text = pun;
         _buttonClose.onClick.AddListener(OnButtonClicked);
         _imageCrown.gameObject.SetActive(isOwner);
+
+        _buttonAccept.onClick.AddListener(profile.Groetjes);
+        _buttonAccept.onClick.AddListener(Dispose);
+
+        _buttonDecline.onClick.AddListener(profile.FijneAvond);
+        _buttonDecline.onClick.AddListener(Dispose);
     }
+
 
     private void OnButtonClicked()
     {
+        Dispose();
+    }
+
+    private void Dispose()
+    {
         _buttonClose.onClick.RemoveListener(OnButtonClicked);
+        _buttonAccept.onClick.RemoveAllListeners();
+        _buttonDecline.onClick.RemoveAllListeners();
         transform.DOMoveX(1620, 0.3f);
 
         View.instance.SetHeader("My hive");
